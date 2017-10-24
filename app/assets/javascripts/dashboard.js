5680fdac3e226fec673ffa58aed0f8c0c681c414
Arthaey <arthaey@gmail.com>
2017-10-24 08:02:19 -0700

    Basic drag and drop for pomodoros.

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,4 +1,21 @@
-function togglePomodoro(e) {
+// https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function addId(element) {
+  if (element.id) {
+    console.error("Element already has an ID: " + element);
+    return;
+  }
+
+  element.id = uuidv4();
+}
+
+function togglePomodoro(ev) {
   var pomodoro = this;
   var wasStopped = pomodoro.classList.contains("stopped");
   var wasStarted = pomodoro.classList.contains("started");
@@ -15,10 +32,46 @@ function togglePomodoro(e) {
   }
 }
 
+function dragPomodoro(ev) {
+  ev.dataTransfer.dropEffect = "move";
+  ev.dataTransfer.setData("text/plain", ev.target.id);
+}
+
+function dropPomodoro(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.classList.remove("drag-hover");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function highlightDragDropZone(ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+  ev.target.classList.add("drag-hover");
+}
+
+function unhighlightDragDropZone(ev) {
+  ev.preventDefault();
+  ev.target.classList.remove("drag-hover");
+}
+
 function init() {
-  var pomodoros = document.querySelectorAll(".pomodoro:not(.stopped)");
-  pomodoros.forEach(function(pomodoro) {
+	/*
+  document.querySelectorAll(".pomodoro:not(.stopped)").forEach(function(pomodoro) {
     pomodoro.addEventListener("click", togglePomodoro);
+  });
+  */
+
+  document.querySelectorAll(".pomodoro").forEach(function(pomodoro) {
+    addId(pomodoro);
+    pomodoro.addEventListener("dragstart", dragPomodoro);
+  });
+
+  document.querySelectorAll(".drag-drop-zone").forEach(function(zone) {
+    zone.addEventListener("drop", dropPomodoro);
+    zone.addEventListener("dragover", highlightDragDropZone);
+    zone.addEventListener("dragexit", unhighlightDragDropZone);
+    zone.addEventListener("dragleave", unhighlightDragDropZone);
   });
 }
 
